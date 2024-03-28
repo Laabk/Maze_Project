6481708c73ef11b3336c18de084ece13a566dbbd
laabk <laabkdavid@gmail.com>
2024-03-28 09:18:22 +0000

    All files

---
+++ maze.c
@@ -0,0 +1,42 @@
+/*
+   File: main.c
+   Author: [Ojeifo David]
+   Description: Simple maze rendering using SDL and raycasting.
+*/
+#include "maze.h"
+
+int width = 1500; 
+int width2d = 820;
+int height = 820;
+int mapW = 15;      
+int mapH = 15;     
+int boxSize = 32; 
+bool running;     
+
+Vector pPos = {70, 70};
+float pA = PI / 2;  
+Vector pd = {0, 0};
+
+int main(int argc, char **argv)
+{
+    sdl_init();
+
+    running = true;
+
+    while (running)
+    {
+        process();
+
+        pd.x = cos(pA);
+        pd.y = sin(pA);
+        clear();
+
+        drawMap();
+        player_drawing();
+
+        sections_drawing();
+        Screen();
+    }
+    sdl_exit();
+    return 0;
+}
